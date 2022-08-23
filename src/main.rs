1674ed60c388a6e91db7373b6100c32cde481bca
tresonic <tresonic@mail.de>
2022-08-23 13:39:04 +0200

    don't bind to localhost

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ async fn main() {
         .route("/get_meters", get(get_meters).layer(Extension(db.clone())))
         .route("/create_entry", post(create_entry).layer(Extension(db.clone())));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
